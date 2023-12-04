0f22117e4372c9cf97c8ca7381fa47466194a72e
Samir <sjavid786@gmail.com>
2023-12-04 14:44:52 -0500

    update

--- 2023/Day 4/Day04_2.py
+++ 2023/Day 4/Day04_2.py
@@ -1,50 +1,50 @@
-def winning_numbers_value (file_path):
+def winning_numbers_value(file_path):
     # Open the specified file in read mode
     with open(file_path, 'r') as file:
         # Read all lines from the file and store them in a list
         game_strings = file.readlines()
-        
-        total_score = get_card_numbers(game_strings)
-        
-        
-    return  total_score
+        # Initialize a dictionary to keep track of the number of cards for each game set
+        line_counts = {index: 1 for index in range(len(game_strings))}
+        # Call the function to calculate the total number of cards
+        total_cards = get_card_numbers(game_strings, line_counts)
 
-def get_card_numbers(game_strings):
+    # Return the total number of cards
+    return total_cards
+
+def get_card_numbers(game_strings, line_counts):
+    # Initialize the total number of cards
     total_cards = 0
     
-    for card in game_strings:
-        total_cards += 1
-    
-    for game_string in game_strings:
+    # Iterate through each game set in the list of game strings
+    for game_set, game in enumerate(game_strings):
+        # Initialize the count of matches for the current game set
         matches = 0
-        colon_index = game_string.index(":")
-        bar_index = game_string.index("|")
-        
-        set1_string = game_string[colon_index + 1:bar_index].strip()
-        set2_string = game_string[bar_index + 1:].strip()
-        
-        winning_numbers = list(map(int, set1_string.split()))
-        card_numbers = list(map(int, set2_string.split()))
-    
+        # Find the indices of the colon and pipe characters
+        colon_index = game.index(":")
+        pipe_index = game.index("|")
+
+        # Directly convert the split strings to integers
+        winning_numbers = list(map(int, game[colon_index + 1:pipe_index].strip().split()))
+        card_numbers = list(map(int, game[pipe_index + 1:].strip().split()))
+
+        # Check for matches between winning and card numbers
         for num in winning_numbers:
-                if num in card_numbers:
-                    matches += 1
-                else:
-                    continue
-        
-        occurrences_count = {num: card_numbers.count(num) for num in winning_numbers}
+            if num in card_numbers:
+                matches += 1
 
-        print("Occurrences Count:", occurrences_count)
-        print(f"Matches: {matches}")
-        #total_points = total_points + score
-        
+        # Update line counts for subsequent games based on the number of matches
+        for card_increase in range(game_set + 1, game_set + matches + 1):
+            line_counts[card_increase] += line_counts[game_set]
+
+        # Update the total number of cards
+        total_cards = sum(line_counts.values())
+
+    # Return the total number of cards
     return total_cards
-    
 
-file_path = "C:\\Users\\sjavi\\OneDrive\\Documents\\Advent of Code\\2023\\Day 4\\Test.txt"
-
-# Call the sum_game_numbers_from_file function with the provided file path
-total_points = winning_numbers_value(file_path)
-
+# Specify the file path
+file_path = "C:\\Users\\sjavi\\OneDrive\\Documents\\Advent of Code\\2023\\Day 4\\Day04.txt"
+# Call the winning_numbers_value function with the provided file path
+total_cards = winning_numbers_value(file_path)
 # Print the result
-print(f"The number of cards is is: {total_points}")
+print(f"The number of cards is: {total_cards}")
